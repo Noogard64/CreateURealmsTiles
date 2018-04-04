dd8fd29f26f4ea438518ff7e930b311d1b519ec9
Sean Phillips <sean-phillips@Live.com>
2018-04-04 19:39:22 -0400

    Initial commit

---
+++ CreateURealmsTiles/Functions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace CreateURealmsTiles
+{
+    class Functions
+    {
+
+        static public string GetGimpExePath()
+        {
+            string gimpLocation = @"C:\Program Files\GIMP 2\bin\gimp-2.8.exe";
+
+            bool gimpFound = false;
+
+            do
+            {
+                if (File.Exists(gimpLocation) == false)
+                {
+                    Console.WriteLine("'gimp-2.8.exe' not found in default location. Please enter location.");
+                    gimpLocation = Console.ReadLine();
+
+                }
+                else
+                {
+                    gimpFound = true;
+                }
+            } while (gimpFound == false);
+            return gimpLocation;
+        }
+
+        static public string GetFile()
+        {
+            bool fileFound = false;
+            string file;
+            do
+            {
+                Console.WriteLine("Input filepath of image:");
+                file = Console.ReadLine();
+                if (File.Exists(file) == false)
+                {
+                    Console.WriteLine("Image not found. Try again.");
+                    file = Console.ReadLine();
+
+                }
+                else
+                {
+                    fileFound = true;
+                }
+
+
+            } while (fileFound == false);
+            return file;
+        }
+    }
+}
---
+++ CreateURealmsTiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace CreateURealmsTiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup
+
+            //Does GIMP exist in the default directory?
+            string gimpExePath = Functions.GetGimpExePath();
+
+            //Execute batfile to run python script
+            string file = Functions.GetFile();
+
+
+
+
+
+        }
+    }
+}
