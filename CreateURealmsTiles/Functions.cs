fe5013800fb46feeec757c3b8d0ea8637035634a
sean phillips <sean-phillips@live.com>
2018-04-04 21:03:10 -0400

    Figured out launching gimp python script.

--- CreateURealmsTiles/Functions.cs
+++ CreateURealmsTiles/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Diagnostics;
 
 namespace CreateURealmsTiles
 {
@@ -32,15 +34,15 @@ namespace CreateURealmsTiles
         {
             bool fileFound = false;
             string file;
+
+            Console.WriteLine("Input filepath of image:");
             do
             {
-                Console.WriteLine("Input filepath of image:");
+                
                 file = Console.ReadLine();
                 if (File.Exists(file) == false)
                 {
                     Console.WriteLine("Image not found. Try again.");
-                    file = Console.ReadLine();
-
                 }
                 else
                 {
@@ -50,6 +52,104 @@ namespace CreateURealmsTiles
 
             } while (fileFound == false);
             return file;
+        }
+
+        static public string[] GetImageFilesAsCollection(string folderPath)
+        {
+            if (folderPath == "")
+            {
+                folderPath = @"C:\Users\sean-\Desktop\urealms\Tiles\Regirock";
+            }
+
+            string[] files = Directory.GetFiles(folderPath);
+
+            return files;
+
+        }
+
+        static public string CreateJSONFileFromTemplate(string folderPath)
+        {
+            var jsonTemplate = @"C:\Users\sean\Desktop\Repos\CreateURealmsTileJSON\CreateURealmsJSONFiles\json_Example.json";
+            String[] newJsonFile = folderPath.Split('\\');
+            var newJsonFileName = folderPath + @"\" + newJsonFile.Last() + ".json";
+            File.Copy(jsonTemplate, newJsonFileName, true);
+            return newJsonFileName;
+        }
+
+
+
+        static public void UpdateJSONFile(string jsonFile, string find, string replace)
+        {
+            string fileContent = File.ReadAllText(jsonFile);
+            string updatedFileContent = fileContent.Replace(find, replace);
+            File.WriteAllText(jsonFile, updatedFileContent);
+
+        }
+
+        static public string textToFindInJSON(string file)
+        {
+
+            string find;
+            if (file.Contains("Blind"))
+            {
+                find = "insert blind url here";
+            }
+            else if (file.Contains("Burning"))
+            {
+                find = "insert burning url here";
+            }
+            else if (file.Contains("Charmed"))
+            {
+                find = "insert charmed url here";
+            }
+            else if (file.Contains("Defeated"))
+            {
+                find = "insert defeated url here";
+            }
+            else if (file.Contains("Frozen"))
+            {
+                find = "insert frozen url here";
+            }
+            else if (file.Contains("Poisoned"))
+            {
+                find = "insert poisoned url here";
+            }
+            else if (file.Contains("Silenced"))
+            {
+                find = "insert silenced url here";
+            }
+            else if (file.Contains("Stunned"))
+            {
+                find = "insert stunned url here";
+            }
+            else
+            {
+                find = "insert base url here";
+            }
+            return find;
+        }
+
+        static public void MakeImages(string file)
+        {
+            try
+            {
+
+                var startInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = @"C:\Users\Sean\Desktop\repos\CreateURealmsTiles\CreateURealmsTiles",
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    FileName = @"C:\Program Files\GIMP 2\bin\gimp-2.8.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    Arguments = "gimp --verbose -idf --batch-interpreter=python-fu-eval -b \"import sys; sys.path =['.'] + sys.path; import batch_CreateURealmsTileImages; batch_CreateURealmsTileImages.run('" + file + "')\" -b \"pdb.gimp_quit(1)\""
+                };
+
+                Process.Start(startInfo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
--- CreateURealmsTiles/Program.cs
+++ CreateURealmsTiles/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace CreateURealmsTiles
 {
@@ -12,13 +13,38 @@ namespace CreateURealmsTiles
             //Does GIMP exist in the default directory?
             string gimpExePath = Functions.GetGimpExePath();
 
-            //Execute batfile to run python script
-            string file = Functions.GetFile();
+            //Get the name of the file
+            string file = @"C:\Users\Sean\Desktop\urealms\urealms_tiles\NPC_Dwarf_01.png"; //Functions.GetFile();
+
+            //Execute GIMP-Python
+            try {
+
+                var startInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = @"C:\Users\Sean\Desktop\repos\CreateURealmsTiles\CreateURealmsTiles",
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    FileName = @"C:\Program Files\GIMP 2\bin\gimp-2.8.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    Arguments = "gimp --verbose -idf --batch-interpreter=python-fu-eval -b \"import sys; sys.path =['.'] + sys.path; import batch_CreateURealmsTileImages; batch_CreateURealmsTileImages.run('" + file + "')\" -b \"pdb.gimp_quit(1)\""
+                };
+
+                Process.Start(startInfo);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
 
 
 
 
 
         }
+
+        
     }
 }
