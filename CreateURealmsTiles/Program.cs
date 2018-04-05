6d9eadd3fafe1312b14b8213b6d293e4183303a3
Sean Phillips <sean-phillips@Live.com>
2018-04-05 10:24:48 -0400

    Finished. Ready for QA.

--- CreateURealmsTiles/Program.cs
+++ CreateURealmsTiles/Program.cs
@@ -14,10 +14,18 @@ namespace CreateURealmsTiles
             string gimpExePath = Functions.GetGimpExePath();
 
             //Get the name of the file
-            string file = @"C:\Users\Sean\Desktop\urealms\urealms_tiles\NPC_Dwarf_01.png"; //Functions.GetFile();
+            string file = Functions.GetFile();
+            //string file = @"C:\Users\sean-\Desktop\urealms\Tiles\Regirock.png"; 
+            //string file = @"C:\Users\Sean\Desktop\urealms\urealms_tiles\NPC_Dwarf_01.png"; 
+
+            //C:\Users\sean-\Desktop\urealms\Tiles\Regirock.png
+
 
             //Execute GIMP-Python
             Functions.MakeImages(gimpExePath, file);
+            Console.WriteLine("When GIMP has finished processing the image, press any key to continue.");
+            Console.ReadKey();
+
 
             //Get Files as a collection
             string[] files = Functions.GetImageFilesAsCollection(file);
@@ -27,19 +35,28 @@ namespace CreateURealmsTiles
 
             foreach (string image in files)
             {
+                if (image.Contains("json") == false)
+                {
+                    Console.WriteLine("#################################");
+                    Console.WriteLine("Uploading [" + image + "] to Imgur.");
+                    var replace = UploadImage.UploadImageToImgur(image);
 
-                var replace = UploadImage.UploadImageToImgur(file);
+                    var find = Functions.textToFindInJSON(image);
 
-                var find = Functions.textToFindInJSON(file);
-                Console.WriteLine(find);
+                    Console.WriteLine("Adding [" + image + "] to JSON file.");
+                    Functions.UpdateJSONFile(newJsonFileName, find, replace);
 
-                Functions.UpdateJSONFile(newJsonFileName, find, replace);
+                    Console.WriteLine("[" + image + "] finished.");
+
+                }
             }
 
+            string folderPath = file.Replace(".png", "");
 
-            Console.WriteLine("Finished!");
+            Console.WriteLine("Finished! Press any key to close this dialog. The location of your new JSON file will open.");
+
             Console.ReadKey();
-
+            Process.Start(folderPath);
 
 
 
---
+++ CreateURealmsTiles/UploadImage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+using Newtonsoft.Json.Linq;
+
+namespace CreateURealmsTiles
+{
+    class UploadImage
+    {
+       
+        static public string UploadImageToImgur(string file)
+        {
+
+            string jsonResults = uploadimage(file);
+            string url = getURLFromJson(jsonResults);
+            return url;
+
+        }
+
+
+        static public string getURLFromJson(string jsonResults)
+        {
+            dynamic stuff = JObject.Parse(jsonResults);
+
+            string url = stuff.data.link;
+            return url;
+        }
+
+        static public string uploadimage(string filename)
+        {
+
+            var file = File.ReadAllBytes(filename);
+
+            using (var w = new WebClient())
+            {
+                var values = new NameValueCollection
+                {
+                    {"image", Convert.ToBase64String(file)},
+                    {"type", "base64"}
+                };
+                //client.Headers.Add("Authorization", "BEARER " + accessToken);
+                w.Headers.Add("Authorization", "Client-ID c927064c3cd35e5");
+                var response = w.UploadValues("https://api.imgur.com/3/image", values);
+
+                string jsonResults = Encoding.UTF8.GetString(response);
+                return jsonResults;
+            }
+
+        }
+    }
+}
